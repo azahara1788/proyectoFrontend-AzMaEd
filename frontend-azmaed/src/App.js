cc2888d845b2e27610cc27ca2f50c420d4b9a0df
Marc <marc.ener.garcia@gmail.com>
2023-02-20 16:29:06 +0100

    App.js

--- frontend-azmaed/src/App.js
+++ frontend-azmaed/src/App.js
@@ -1,32 +1,97 @@
-import { Theme } from "./components/Theme";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import Theme from "./components/Theme";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "./App.css";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { useThemeContext } from "./context/ThemeContext";
 import { UserPage } from "./pages/UserPage";
 import ErrorBoundary from "./ErrorBoundary";
+import { PrivateRoute } from "./components/PrivateRoute";
+import { Header } from "./components/Header";
+import { LoginPage } from "./pages/LoginPage";
+import { HomePage } from "./pages/HomePage";
+import { NotesUserPage } from "./pages/NotesUserPage";
+import { NotePage } from "./pages/NotePage";
+import { UserRoute } from "./components/UserRoute";
+import { CategoryPage } from "./pages/CategoryPage";
+import { NoteAllPage } from "./pages/NoteAllPage";
 
 function App() {
   const { theme } = useThemeContext();
   const location = useLocation();
   return (
     <>
+      <Header />
       <main className="App" id={theme}>
-        <Theme />
         <ErrorBoundary
           key={location.pathname}
           fallback={<h2>Error en la section</h2>}
         >
           <Routes>
-            <Route path="/user/:id" element={<UserPage />} />
-            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/note"
+              element={
+                <PrivateRoute>
+                  <NotesUserPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/notes"
+              element={
+                <PrivateRoute>
+                  <NotesAllPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/notes/category/:id"
+              element={
+                <PrivateRoute>
+                  <CategoryPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/note/:id"
+              element={
+                <PrivateRoute>
+                  <NotePage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <UserRoute>
+                  <LoginPage />
+                </UserRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <UserRoute>
+                  <RegisterPage />
+                </UserRoute>
+              }
+            />
+            <Route
+              path="/user"
+              element={
+                <PrivateRoute>
+                  <UserPage />
+                </PrivateRoute>
+              }
+            />
             <Route /* path="*" */ path="/notfound" element={<NotFoundPage />} />
           </Routes>
         </ErrorBoundary>
       </main>
+      <Footer />
       <ToastContainer position="bottom-center" pauseOnHover theme="dark" />
     </>
   );
